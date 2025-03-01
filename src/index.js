f3c6a89b0a1e98b7d219d36ab9a69dfce6203957
anmol3538 <anmolaggarwal557@gmail.com>
2025-03-01 23:19:42 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const {PORT} = require("./config/serverconfig")
+
 const setupandstartserver = async () => {
     const app = express();
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}))
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
     })
